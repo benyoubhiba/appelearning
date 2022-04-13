78ead3ffb281884e2b96d31328da9f5ba4b3a6dd
abradi1 <chaima.abradi@etu.unilim.fr>
2022-04-13 16:24:53 +0200

    test_code

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -32,11 +34,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 255)]
     private $prenom;
 
-    #[ORM\OneToMany(mappedBy: 'enseignant', targetEntity: Classe::class)]
+    /*#[ORM\OneToMany(mappedBy: 'enseignant', targetEntity: Classe::class)]
     private $classes;
 
     #[ORM\ManyToOne(targetEntity: Cours::class, inversedBy: 'id_professeur')]
-    private $cours;
+    private $cours;*/
 
     public function __construct()
     {
